6538c71feeba0109857884b2b4a74a8fa53cf45c
Peter Hæstrup <phae@itu.dk>
2025-08-29 09:52:58 +0200

    Display for Cheeps

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿List<String> cheeps = new(File.ReadAllLines("../../../chirp_cli_db.csv"));
+cheeps.RemoveAt(0);
+
+foreach (String cheep in cheeps)
+{
+    String[]  split = cheep.Split('\"');
+    String user = split[0].Replace(",", "");
+    String message = split[1];
+    String date = split[2].Replace(",", "");
+    Console.WriteLine(user+" @ "+date+": "+message);
+}
