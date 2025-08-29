d2b1473ed67aebed5d1810a213f765bff10f9069
Peter Hæstrup <phae@itu.dk>
2025-08-29 10:25:47 +0200

    Can now cheep from cmd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿List<String> cheeps = new(File.ReadAllLines("../../../chirp_cli_db.csv"));
-cheeps.RemoveAt(0);
+﻿if (args.Length == 0) return;
 
-foreach (String cheep in cheeps)
+if (args[0] == "read") {
+    List<String> cheeps = new(File.ReadAllLines("chirp_cli_db.csv"));
+
+    foreach (String cheep in cheeps) {
+        String[]  split = cheep.Split('\"');
+        if (split.Length < 2) continue;
+        String user = split[0].Replace(",", "");
+        String message = split[1];
+        DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(int.Parse(split[2].Replace(",", "")));
+        String date = "" + time;
+        Console.WriteLine(user+" @ "+date.Remove(date.Length-7)+": "+message);
+    }
+}else if (args[0] == "cheep")
 {
-    String[]  split = cheep.Split('\"');
-    String user = split[0].Replace(",", "");
-    String message = split[1];
-    String date = split[2].Replace(",", "");
-    Console.WriteLine(user+" @ "+date+": "+message);
+    if (args.Length < 2) return;
+    String message = args[1];
+    File.AppendAllText("chirp_cli_db.csv","\n"+Environment.UserName + ",\"" + message + "\"," + DateTimeOffset.UtcNow.ToUnixTimeSeconds());
 }
