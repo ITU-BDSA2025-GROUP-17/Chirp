5aff8fea62f77dd57f45cdab950b1f71b513a4ef
Peter Hæstrup <phae@itu.dk>
2025-08-29 10:41:51 +0200

    Refactoring & time fix

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿if (args.Length == 0) return;
 
 if (args[0] == "read") {
-    List<String> cheeps = new(File.ReadAllLines("chirp_cli_db.csv"));
-
-    foreach (String cheep in cheeps) {
-        String[]  split = cheep.Split('\"');
-        if (split.Length < 2) continue;
-        String user = split[0].Replace(",", "");
-        String message = split[1];
-        DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(int.Parse(split[2].Replace(",", "")));
-        String date = "" + time;
-        Console.WriteLine(user+" @ "+date.Remove(date.Length-7)+": "+message);
-    }
+    Read();
 }else if (args[0] == "cheep")
 {
     if (args.Length < 2) return;
-    String message = args[1];
-    File.AppendAllText("chirp_cli_db.csv","\n"+Environment.UserName + ",\"" + message + "\"," + DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+    Cheep(args[1]);
+}
+
+void Read() 
+{
+    List<String> cheeps = new(File.ReadAllLines("chirp_cli_db.csv"));
+
+    foreach (String currentCheep in cheeps) {
+        String[]  split = currentCheep.Split('\"');
+        if (split.Length < 2) continue;
+        String user = split[0].Replace(",", "");
+        String message = split[1];
+        DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(int.Parse(split[2].Replace(",", ""))).LocalDateTime;
+        String date = "" + time;
+        Console.WriteLine(user+" @ "+date.Remove(date.Length-7)+": "+message);
+    }
+}
+
+void Cheep(String message)
+{
+    String name = Environment.UserName;
+    long time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();  
+    File.AppendAllText("chirp_cli_db.csv","\n"+ name + ",\"" + message + "\"," + time);
 }
