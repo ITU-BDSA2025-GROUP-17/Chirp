6b56dd8d58895a36e52a2019afd7f1d2e2176171
Rasmus Bondo <rabh@itu.dk>
2025-09-05 11:25:36 +0200

    database now resides in another library project

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Runtime.InteropServices.Marshalling;
+﻿using System.Globalization;
 using CsvHelper;
+using SimpleDB;
 
 class Program
 {
+    public static CSVDatabase<Cheep> db = new SimpleDB.CSVDatabase<Cheep>();
     static void Main(string[] args)
     {
-        String filePath = "C:\\Users\\2000b\\Documents\\Chirp\\bin\\chirp_cli_db.csv";
         if (args.Length == 0)
         {
             Console.WriteLine("No arguments given. Exiting...");
@@ -18,59 +15,34 @@ class Program
 
         if (args[0] == "read")
         {
-            read(filePath);
+            read(int.Parse(args[1]));
             return; // skip the rest of Main
         }
         
         if (args[0] == "cheep")
         {
-            cheep(filePath, args[1]);
+            cheep(args[1]);
         }
     }
 
-    static void read(string path)
+    static void read(int limit)
     {
-        using (var reader = new StreamReader(path))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        var chirps = db.Read(limit);
+        
+        foreach (var cheep in chirps)
         {
-            var chirps = csv.GetRecords<Cheep>();
-            
-            foreach (Cheep cheep in chirps)
-            {
-                DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp).DateTime;
+            DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp).DateTime;
 
-                Console.WriteLine(cheep.Author + " @ " + dateTime + ": " + cheep.Message);
-            }
-
+            Console.WriteLine(cheep.Author + " @ " + dateTime + ": " + cheep.Message);
         }
     }
 
-    static void cheep(string path, string message)
+    static void cheep(string message)
     {   
         string username = Environment.UserName;
         long unixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        string timestampString = unixTime.ToString();
-
-        var chirps = new List<Cheep>();
         
-        using (var reader = new StreamReader(path))
-        using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            var savedChirps = csvReader.GetRecords<Cheep>();
-
-            foreach (Cheep cheep in savedChirps)
-            {
-                chirps.Add(cheep);
-            }
-            chirps.Add(new(username, message, unixTime));
-        }
-        
-        using (var writer = new StreamWriter(path))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-        {
-            csv.WriteRecords(chirps);
-            writer.Flush();
-        }
+        db.Store(new Cheep(username, message, unixTime));
     }
     
 }
