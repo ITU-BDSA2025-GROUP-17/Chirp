dd64e8d5013a6f91cddf785a1e727d47a884b22f
Peter Hæstrup <phae@itu.dk>
2025-09-10 14:41:46 +0200

    repo merge fix

---
+++ simpleDB/CSVDatabase.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+using CsvHelper;
+
+namespace simpleDB;
+
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    String filepath;
+    public CSVDatabase(string filepath)
+    {
+        this.filepath = filepath;
+    }
+
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using (StreamReader reader = new StreamReader(filepath))
+        using (CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var result = csvReader.GetRecords<T>().ToList();
+            if (limit == null) {  return result; }
+            result.RemoveRange(0, result.Count - (int)limit);
+            return result;
+        }
+    }
+
+    public void Store(T record)
+    {
+        List<T> records = Read().ToList();
+        records.Add(record);
+
+        using (StreamWriter writer = new StreamWriter(filepath))
+        using (CsvWriter csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csvWriter.WriteRecords(records);
+            writer.Flush();
+        }
+    }
+}
---
+++ simpleDB/IDatabaseRepository.cs
@@ -0,0 +1,7 @@
+namespace simpleDB;
+
+public interface IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null);
+    public void Store(T record);
+}
