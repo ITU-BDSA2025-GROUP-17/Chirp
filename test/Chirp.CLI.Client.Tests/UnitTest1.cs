0e04c27fae078aa43599e9b70aaf1690c203144a
Lobo <dsab@itu.dk>
2025-09-12 11:14:48 +0200

    End-2-end test, probaly misplaced

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -7,4 +7,15 @@ public class UnitTest1
     {
 
     }
+
+    [Fact]
+    public void TestReadTenCheeps()
+    {
+        // Arrange
+        var args = new string[] { "read", "10" };
+        // Act
+        var result = Program.Main(args);
+        // Assert
+        Assert.Equal(0, result);
+    }
 }
