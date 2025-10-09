ab35daf7fe337e2a586251ed8cf66b217ab3aafc
A-thenaa <atwi@itu.dk>
2025-10-09 10:11:24 +0200

    fixed bug wih next and prev button on @Author pages

---
+++ test/Chirp.Razor.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+using Xunit;
+
+namespace Chirp.Razor.Tests;
+using Chirp.Razor.wwwroot;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Read_ReturnsCheeps_FromInMemoryDatabase()
+    {
+        // Arrange
+        
+
+        // Act
+        
+
+        // Assert
+        
+    }
+}
