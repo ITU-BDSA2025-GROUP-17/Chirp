9d11d40745dc1805d703e7d0bc2caa3fdc99fc38
Lobo <dsab@itu.dk>
2025-12-03 10:42:13 +0100

    ensuring unfollow button on following page

--- src/Chirp.Web/Pages/Following.cshtml.cs
+++ src/Chirp.Web/Pages/Following.cshtml.cs
@@ -11,6 +11,9 @@ public class FollowingModel : PageModel
 
     public ICollection<AuthorDTO>? Following { get; set; }
 
+    [BindProperty]
+    public string? Unfollow { get; set; }
+
     public FollowingModel(IAuthorRepository authorRepository)
     {
         _authorRepository = authorRepository;
@@ -35,6 +38,22 @@ public class FollowingModel : PageModel
         }
 
         return Page();
+    }
+
+    public async Task<ActionResult> OnPostUnfollowAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var user = User.Identity?.Name;
+        var author = await _authorRepository.GetAuthorByName(user!);
+        var followAuthor = await _authorRepository.GetAuthorByName(Unfollow!);
+        await _authorRepository.UnFollow(author!, followAuthor!);
+
+
+        return RedirectToPage("/Following");
     }
 
 }
