43c86078579d0dafed8459c84c4dae7ad0219a78
A-thenaa <atwi@itu.dk>
2026-01-01 20:06:34 +0100

    Added pdf diagrams

--- src/Chirp.Core/Follow.cs
+++ src/Chirp.Core/Follow.cs
@@ -1,8 +1,5 @@
 namespace Chirp.Core;
 
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 /// <summary>
 /// Represents a follow relationship between two authors.
 /// One author (Follower) follows another author (Following).
@@ -14,7 +11,7 @@ public class Follow
     /// </summary>
     public int FollowerId { set; get; }
     /// <summary>
-    /// The author author who follows another author.
+    /// The author who follows another author.
     /// </summary>
     public Author? Follower { set; get; }
     /// <summary>
@@ -22,7 +19,7 @@ public class Follow
     /// </summary>
     public int FollowingId { set; get; }
     /// <summary>
-    /// The author author who who is followed by another author.
+    /// The  author  who is followed by another author.
     /// </summary>
     public Author? Following { set; get; }
 }
